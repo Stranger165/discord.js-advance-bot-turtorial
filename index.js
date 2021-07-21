b178b646641af604e5e2738dc6c394ddec016455
cortex <75237415+Stranger165@users.noreply.github.com>
2021-07-21 12:14:11 +0530

    🥫 updated and clean

--- index.js
+++ index.js
@@ -1,66 +1,44 @@
-const {Collection, Client, Discord, MessageEmbed} = require('discord.js')
-const fs = require('fs')
-const mongoose = require("mongoose")
-const client = new Client({
-    disableEveryone: true
-})
-
-
-const config = require('./config.json')
-
-const {Collection, Client, Discord, MessageEmbed} = require('discord.js')
-const fs = require('fs')
-const mongoose = require("mongoose")
-const client = new Client({
-    disableEveryone: true
-})
-
-
-const config = require('./config.json')
-
-
-mongoose.connect("paste your mongo", {
+onst {
+    Collection,
+    Client,
+    MessageEmbed,
+    Message,
+    Intents,
+  } = require("discord.js");
+  require("discord-reply");
+  const client = new Client({
+      disableMentions: "everyone",
+  });
+  require('discord-buttons')(client)
+  const path = require("path");
+  const fs = require("fs");
+  const config = require("./config.json");
+  module.exports = client;
+  client.color = config.color;
+  client.commands = new Collection();
+  client.prefix = config.prefix;
+  client.aliases = new Collection();
+  client.categories = fs.readdirSync(path.resolve("./commands"));
+  ["command"].forEach((handler) => {
+    require(path.resolve(`./handlers/${handler}`))(client);
+  });
+  
+  const dbOptions = {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(console.log("connected to mongoDB"))
+    useUnifiedTopology: true,
+    autoIndex: false,
+    poolSize: 5,
+    connectTimeoutMS: 10000,
+    family: 4,
+  };
+  const chalk = require(`chalk`);
+  const { connect } = require("mongoose");
+  connect(config.mongo, dbOptions).then(() => {
+    console.log(
+      chalk.cyan("[Information] ") + chalk.blue(`Connected to Mongodb`)
+    );
+  });
+  
+  client.login(config.token);
 
-
-
-module.exports = client;
-
-const prefix = config.prefix
-const token = config.token
-client.prefix = config.prefix;
-
-
-client.commands = new Collection();
-client.aliases = new Collection();
-
-client.categories = fs.readdirSync("./commands/");
-["command"].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
-}); 
-
-
-client.login(token)
-
-
-
-
-module.exports = client;
-
-const prefix = config.prefix
-const token = config.token
-client.prefix = config.prefix;
-
-
-client.commands = new Collection();
-client.aliases = new Collection();
-
-client.categories = fs.readdirSync("./commands/");
-["command"].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
-}); 
-
-
-client.login(token)
+  // code link in description
